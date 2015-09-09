33012d50680599de93fcb34d03831dcad8efbcf3
ethan <ethanmannette@gmail.com>
2015-09-09 10:37:47 -0600

    replicated angular text box with vanilla javascript

--- vanilla.js
+++ vanilla.js
@@ -1,1 +1,8 @@
 var text = document.getElementById('name');
+var button = document.getElementById('button');
+var header = document.getElementById('text');
+
+button.addEventListener('click', function(e) {
+  e.preventDefault();
+  header.innerText = header.innerText += " " + text.value;
+});
