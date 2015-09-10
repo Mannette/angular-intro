e9c0d5a65a6796472cd17b3e2df351f35d898743
ethan <ethanmannette@gmail.com>
2015-09-10 11:50:18 -0600

    Used angular to iterate through array with duplicates and to iterate over keys to display values

--- angular.js
+++ angular.js
@@ -2,12 +2,12 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('hogwarts', ['$scope', function($scope) {
   $scope.names = [
-    "Harry",
-    "Ron",
-    "Hermione",
-    "Sirius",
-    "Hedwig",
-    "Tonks"
+    'Harry',
+    'Ron',
+    'Hermione',
+    'Sirius',
+    'Hedwig',
+    'Tonks'
   ];
 
 }]);
@@ -18,5 +18,28 @@ myApp.controller('symbols', ['$scope', function($scope) {
     '\u2663',
     '\u2665',
     '\u25C6'
+  ];
+
+}]);
+
+myApp.controller('numbers', ['$scope', function($scope) {
+  $scope.nums = [
+    1,
+    1,
+    2,
+    5,
+    6,
+    9,
+    9,
+    9
+  ];
+}]);
+
+myApp.controller('person', ['$scope', function($scope) {
+  $scope.students = [
+    {name: 'Bae', age: 20},
+    {name: 'John', age: 27},
+    {name: 'Greg', age: 25},
+    {name: 'Fred', age: 23}
   ];
 }]);
