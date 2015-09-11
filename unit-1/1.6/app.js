be2bd1e16ae957f29e715c5c5240d6b64d849cd4
ethan <ethanmannette@gmail.com>
2015-09-11 12:14:50 -0600

    refactored madLibs to use a controller.

--- unit-1/1.6/app.js
+++ unit-1/1.6/app.js
@@ -3,3 +3,22 @@ var app = angular.module('firstApp', []);
 app.controller('MyFirstController', function($scope) {
   $scope.name = 'Severus Snape';
 });
+
+app.controller('ExercisesController', function($scope) {
+  $scope.favColor = 'blue';
+  $scope.secondsInACentury = 100 * 365 * 24 * 60 * 60;
+
+  var today = new Date();
+  $scope.rightNow = today.toLocaleDateString();
+});
+
+app.controller('madLibs', function($scope) {
+  $scope.libs = function() {
+    $scope.name = document.getElementById('boyName').value;
+    $scope.adjective = document.getElementById('adjective').value;
+    $scope.firstNoun = document.getElementById('firstNoun').value;
+    $scope.secondNoun = document.getElementById('secondNoun').value;
+    $scope.insect = document.getElementById('insect').value;
+    $scope.verb = document.getElementById('verb').value;
+  }
+});
